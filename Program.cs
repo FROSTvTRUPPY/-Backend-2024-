f41806041f8ead24e9036c2f239de5cf274e8805
FROSTvTRUPPY <spdier25559@bk.ru>
2024-05-27 15:22:00 +0400

    Zadanie_4

--- Program.cs
+++ Program.cs
@@ -1,16 +1,57 @@
-﻿{
-    List<int> numbers = new List<int> { 2, 7, 4, 1, 8 };
-    Console.WriteLine("Наибольшая сумма двух чисел в списке: " + FindLargestSum(numbers)[1] + " + " + FindLargestSum(numbers)[0] + " = " + FindLargestSum(numbers)[2]);
+﻿Console.Write("Введите целые числа через пробел:");
+
+string input = Console.ReadLine(); // Чтение ввода пользователя и разделение его на числа
+string[] numbersStr = input.Split(' ');
+int[] numbers = new int[numbersStr.Length];
+
+
+for (int i = 0; i < numbersStr.Length; i++)
+{
+    numbers[i] = int.Parse(numbersStr[i]); // Преобразование строковых значений в целые числа
 }
 
-static int[] FindLargestSum(List<int> numbers)
+SortArray(numbers);
+
+Console.Write("\nОтсортированный массив:");
+foreach (int num in numbers)
 {
-    if (numbers.Count < 2)
+    Console.Write(num + " ");
+}
+
+Console.Write("\nВведите искомое число:");
+
+string input2 = Console.ReadLine(); // Чтение ввода пользователя и разделение его на числа
+int target;
+target = int.Parse(input2); // Преобразование строковых значений в целые числа
+Console.WriteLine("Индекс элемента " + target + " в списке: [" + BinarySearch(numbers, target) + "]");
+
+static void SortArray(int[] arr)
+{
+    Array.Sort(arr);
+}
+
+static int BinarySearch(int[] numbers, int target)
+{
+    int left = 0;
+    int right = numbers.Length - 1;
+
+    while (left <= right)
     {
-        throw new ArgumentException("Список должен содержать как минимум два элемента.");
+        int mid = left + (right - left) / 2;
+
+        if (numbers[mid] == target)
+        {
+            return mid;
+        }
+        else if (numbers[mid] < target)
+        {
+            left = mid + 1;
+        }
+        else
+        {
+            right = mid - 1;
+        }
     }
 
-    numbers.Sort();
-    int[] nums= { numbers[numbers.Count - 1], numbers[numbers.Count - 2], numbers[numbers.Count - 1] + numbers[numbers.Count - 2] };
-    return nums;
+    return -1; // Элемент не найден
 }
