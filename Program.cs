08f3d8e85ec0059310454aa030fa685d6751c7a3
FROSTvTRUPPY <spdier25559@bk.ru>
2024-05-27 15:20:46 +0400

    Zadanie 3

--- Program.cs
+++ Program.cs
@@ -1,52 +1,16 @@
-﻿int[] A = { 2, 4, 1, 3, 2, 4, 6, 7, 9, 2, 19 };
-int[] B = { 2, 1, 4, 3, 6, 9 };
-
-Console.Write("    Изначальный массив A: ");
-foreach (int num in A)
-{
-    Console.Write(num + " ");
+﻿{
+    List<int> numbers = new List<int> { 2, 7, 4, 1, 8 };
+    Console.WriteLine("Наибольшая сумма двух чисел в списке: " + FindLargestSum(numbers)[1] + " + " + FindLargestSum(numbers)[0] + " = " + FindLargestSum(numbers)[2]);
 }
 
-Console.Write("\n                массив B: ");
-foreach (int num in B)
+static int[] FindLargestSum(List<int> numbers)
 {
-    Console.Write(num + " ");
-}
-
-SortArrayByOrder(A, B);
-
-Console.Write("\nОтсортированный массив A: ");
-foreach (int num in A)
-{
-    Console.Write(num + " ");
-}
-
-static void SortArrayByOrder(int[] A, int[] B)
-{
-    Dictionary<int, int> indexMap = new Dictionary<int, int>();
-
-    for (int i = 0; i < B.Length; i++)// Создаем карту индексов элементов из B в A
+    if (numbers.Count < 2)
     {
-        indexMap[B[i]] = i;
+        throw new ArgumentException("Список должен содержать как минимум два элемента.");
     }
 
-    Array.Sort(A, (x, y) =>
-    {
-        if (indexMap.ContainsKey(x) && indexMap.ContainsKey(y))
-        {
-            return indexMap[x].CompareTo(indexMap[y]);// Если оба элемента присутствуют в B, сравниваем их порядок в B
-        }
-        else if (indexMap.ContainsKey(x))
-        {
-            return -1;// Если только x присутствует в B, то он должен быть раньше
-        }
-        else if (indexMap.ContainsKey(y))
-        {
-            return 1;// Если только y присутствует в B, то y должен быть раньше
-        }
-        else
-        {
-            return y.CompareTo(x);// Если ни x, ни y не присутствуют в B, сортируем их по убыванию
-        }
-    });
+    numbers.Sort();
+    int[] nums= { numbers[numbers.Count - 1], numbers[numbers.Count - 2], numbers[numbers.Count - 1] + numbers[numbers.Count - 2] };
+    return nums;
 }
